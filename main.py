951fbf89dba885bf7ccb377eb937f64812e86a9b
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-12-04 17:14:46 +0000

    Added checking for arguments

--- main.py
+++ main.py
@@ -3,4 +3,8 @@
 import os, sys
 from PIL import Image
 
+if (len(sys.argv) < 3):
+    print "Usage: ./main.py <file/folder to process> <destination for corrupt images>"
+    exit(1)
+
 jpgfile = Image.open(sys.argv[1])
